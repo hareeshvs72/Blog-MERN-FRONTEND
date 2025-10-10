b08326787ebfe7fc0a3c58d5084f9b050ec348e9
hareeshvs72 <hareeshvs72@gmail.com>
2025-10-10 09:07:12 +0530

    animation added to user page

--- src/User/Pages/AboutUs.jsx
+++ src/User/Pages/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import { Link } from 'react-router-dom'
+import { easeIn, motion } from 'framer-motion'
 
 function AboutUs() {
   return (
@@ -11,22 +12,39 @@ function AboutUs() {
 
         <div className='md:px-20 md:py-10 p-5 bg-green-100'>
           <div className='flex items-center w-full flex-col'>
-            <h1 className='text-3xl font-bold  my-3'>
+            <motion.h1 
+             initial={{opacity:0, y:-30}}
+             whileInView={{opacity:1,y:0}}
+             transition={{duration:.5, ease:easeIn}}
+            className='text-3xl font-bold  my-3'>
              About Us
-            </h1>
+            </motion.h1>
 
-            <p className='font-semibold text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempora quibusdam culpa adipisci incidunt et numquam blanditiis corrupti eaque sed nulla consectetur facilis neque voluptate magni reprehenderit, quo aliquid molestias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum praesentium libero porro earum quia, fuga perspiciatis laboriosam eius. Neque accusantium exercitationem atque enim excepturi error? Harum quod id delectus necessitatibus.</p>
+            <motion.p 
+              initial={{opacity:0, x:-30}}
+             whileInView={{opacity:1,x:0}}
+             transition={{duration:.5, ease:easeIn}}
+            
+            className='font-semibold text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempora quibusdam culpa adipisci incidunt et numquam blanditiis corrupti eaque sed nulla consectetur facilis neque voluptate magni reprehenderit, quo aliquid molestias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum praesentium libero porro earum quia, fuga perspiciatis laboriosam eius. Neque accusantium exercitationem atque enim excepturi error? Harum quod id delectus necessitatibus.</motion.p>
 
           </div>
 
           <div className="md:grid grid-cols-2 gap-20 my-7 md:px-30 md:py-10">
              {/* image */}
-             <div className='min-h-[500px] '>
+             <motion.div
+               initial={{opacity:0, y:-30}}
+             whileInView={{opacity:1,y:0}}
+             transition={{duration:.5, ease:easeIn}}
+             className='min-h-[500px] '>
                <img src="/aboutimg.jpg" alt="about Image" className='w-full h-full rounded-2xl object-cover' />
-               </div>
+               </motion.div>
 
             {/* content */}
-            <div className="flex flex-col justify-center">
+            <motion.div 
+              initial={{opacity:0, x:30}}
+             whileInView={{opacity:1,x:0}}
+             transition={{duration:.5, ease:easeIn}}
+             className="flex flex-col justify-center">
               <p className='font-semibold text-justify my-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam molestias dicta non? Dolorem, vitae cupiditate aliquid autem officiis a itaque, consequuntur quibusdam voluptatum, quaerat ipsa optio eum id porro necessitatibus.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cumque aliquid, molestias eius suscipit aspernatur numquam porro ab nesciunt ad deleniti quasi voluptates repellat fugiat tempora doloribus soluta architecto quis. </p>
                 <p className='font-semibold text-justify my-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam molestias dicta non? Dolorem, vitae cupiditate aliquid autem officiis a itaque, consequuntur quibusdam voluptatum, quaerat ipsa optio eum id porro necessitatibus.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cumque aliquid, molestias eius suscipit aspernatur numquam porro ab nesciunt ad deleniti quasi voluptates repellat fugiat tempora doloribus soluta architecto quis. </p>
 
@@ -34,7 +52,7 @@ function AboutUs() {
                 <button className='px-4 py-2 bg-black rounded-2xl text-green-400 border-2 border-transparent hover:border-2 hover:border-black hover:bg-green-400 hover:text-black font-bold'><Link to={'/contact'}>Contact Info</Link></button>
               </div>
 
-            </div>
+            </motion.div>
           </div>
 
 
--- src/User/Pages/Contact.jsx
+++ src/User/Pages/Contact.jsx
@@ -6,7 +6,8 @@ import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 import { faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-
+import { easeIn, motion } from 'framer-motion'
+easeIn
 function Contact() {
   return (
     <>
@@ -15,17 +16,29 @@ function Contact() {
 
         <div className='md:px-20 md:py-10 p-5 bg-green-100'>
           <div className='flex items-center w-full flex-col'>
-            <h1 className='text-3xl font-bold  my-3'>
+            <motion.h1 
+                 initial={{opacity:0}}
+             whileInView={{opacity:1}}
+             transition={{duration:.5, ease:easeIn}}
+            className='text-3xl font-bold  my-3'>
               Contact Us
-            </h1>
+            </motion.h1>
 
-            <p className='font-semibold text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempora quibusdam culpa adipisci incidunt et numquam blanditiis corrupti eaque sed nulla consectetur facilis neque voluptate magni reprehenderit, quo aliquid molestias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum praesentium libero porro earum quia, fuga perspiciatis laboriosam eius. Neque accusantium exercitationem atque enim excepturi error? Harum quod id delectus necessitatibus.</p>
+            <motion.p
+                 initial={{opacity:0, x:-30}}
+             whileInView={{opacity:1,x:0}}
+             transition={{duration:.5, ease:easeIn}}
+            className='font-semibold text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempora quibusdam culpa adipisci incidunt et numquam blanditiis corrupti eaque sed nulla consectetur facilis neque voluptate magni reprehenderit, quo aliquid molestias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum praesentium libero porro earum quia, fuga perspiciatis laboriosam eius. Neque accusantium exercitationem atque enim excepturi error? Harum quod id delectus necessitatibus.</motion.p>
 
           </div>
 
           <div className="md:grid grid-cols-2 gap-20 my-7 md:px-30 md:py-10">
             {/* form */}
-            <div className="bg-black w-full p-4  rounded  h-full">
+            <motion.div
+                initial={{opacity:0, x:-30}}
+             whileInView={{opacity:1,x:0}}
+             transition={{duration:.5, ease:easeIn}}
+             className="bg-black w-full p-4  rounded  h-full">
               <form action="">
                 <h1 className="text-center text-green-400 font-bold my-3">Send Me Message</h1>
                 <input
@@ -46,35 +59,47 @@ function Contact() {
                   Send <FontAwesomeIcon icon={faPaperPlane} />
                 </button>
               </form>
-            </div>
+            </motion.div>
 
             {/* social media links */}
             <div className="md:mt-0 my-2 md:h-full h-96 flex items-center justify-center">
               <div  >
-                <div className='flex  cursor-pointer my-5'>
+                <motion.div 
+                   initial={{opacity:0, y:-30}}
+             whileInView={{opacity:1,y:0}}
+             transition={{duration:.5, ease:easeIn}}
+                className='flex  cursor-pointer my-5'>
                   <FontAwesomeIcon icon={faEnvelope} className='bg-black text-green-400 rounded-full p-3 hover:bg-green-400 hover:text-black hover:border-2 border-black ' style={{ width: '20px', height: '20px', borderRadius: '50%' }} />
                   <div className='mx-4'>
                     <h3 className='font-bold'>Email</h3>
                     <p>FeatureTalks@gmail.com</p>
                   </div>
 
-                </div>
-                <div className='flex cursor-pointer my-5'>
+                </motion.div>
+                <motion.div 
+                     initial={{opacity:0, y:-30}}
+             whileInView={{opacity:1,y:0}}
+             transition={{duration:.6, ease:easeIn}}
+                className='flex cursor-pointer my-5'>
                   <FontAwesomeIcon icon={faXTwitter} className='bg-black text-green-400 rounded-full p-3 hover:bg-green-400 hover:text-black hover:border-2 border-black ' style={{ width: '20px', height: '20px', borderRadius: '50%' }} />
                   <div className='mx-4'>
                     <h3 className='font-bold'>Twitter X</h3>
                     <p>@FeatureTalks</p>
                   </div>
 
-                </div>
-                <div className='flex cursor-pointer my-5'>
+                </motion.div>
+                <motion.div 
+                     initial={{opacity:0, y:-30}}
+             whileInView={{opacity:1,y:0}}
+             transition={{duration:.7, ease:easeIn}}
+                className='flex cursor-pointer my-5'>
                   <FontAwesomeIcon icon={faInstagram} className='bg-black text-green-400 rounded-full p-3 hover:bg-green-400 hover:text-black hover:border-2 border-black ' style={{ width: '20px', height: '20px', borderRadius: '50%' }} />
                   <div className='mx-4'>
                     <h3 className='font-bold'>InstaGram</h3>
                     <p>@Feature_Talks</p>
                   </div>
 
-                </div>
+                </motion.div>
 
 
 
